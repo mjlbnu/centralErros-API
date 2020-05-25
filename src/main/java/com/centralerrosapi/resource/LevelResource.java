b36aab8b01ecdea4494708c992c57e5a2fe0c7a3
mjlbnu <mjlbnu@gmail.com>
2020-05-25 00:58:31 -0300

    Criar e buscar Level

--- src/main/java/com/centralerrosapi/resource/LevelResource.java
+++ src/main/java/com/centralerrosapi/resource/LevelResource.java
@@ -3,11 +3,10 @@ package com.centralerrosapi.resource;
 import java.net.URI;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +15,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.centralerrosapi.model.Level;
 import com.centralerrosapi.repository.LevelRepository;
+
 
 @RestController
 @RequestMapping("/levels")
@@ -30,13 +30,18 @@ public class LevelResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Level> criar(@RequestBody Level level, HttpServletResponse response) {
+	public ResponseEntity<Level> criar(@RequestBody Level level) {
 		Level novoLevel = levelRepository.save(level);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{/id}")
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
 				.buildAndExpand(novoLevel.getId()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
 		
 		return ResponseEntity.created(uri).body(novoLevel);
 	}
+	
+	@GetMapping("/{id}")
+	public Level buscar(@PathVariable Long id) {
+		return levelRepository.findById(id).get();
+	}
+
 }
