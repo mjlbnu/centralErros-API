cd493f35fd4c43da52438e1840c9f9158f6ce330
mjlbnu <mjlbnu@gmail.com>
2020-06-23 15:40:13 -0300

    Feature: Moved refresh cookie to request

--- src/main/java/com/centralerrosapi/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/centralerrosapi/token/RefreshTokenPostProcessor.java
@@ -1,7 +1,6 @@
 package com.centralerrosapi.token;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
